8227c60b933121d11bc87f42541d2799e927d60a
JIAN DING <45487811+SDJ-0@users.noreply.github.com>
2020-02-20 16:08:36 +0800

    Add files via upload

---
+++ HomeWork1/Project1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static int Prase(string str)
+        {
+            int n = -1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+                if (c == '+' || c == '-' || c == '*' || c == '/')
+                    n = n == -1 ? i : -1;
+            }
+            return n;
+        }
+        static void Main(string[] args)
+        {
+            string str;
+            int a, b, num;
+            char c;
+            double ans;
+            while (true)
+            {
+                str = Console.ReadLine();
+                if (str == "")
+                {
+                    Console.WriteLine("Over");
+                    Console.ReadKey();
+                    return;
+                }
+                num = Prase(str);
+                if (num == -1)
+                {
+                    Console.WriteLine("Error");
+                    Console.ReadKey();
+                    return;
+                }
+                c = str[num];
+                a = Int32.Parse(str.Remove(num));
+                b = Int32.Parse(str.Substring(num + 1, str.Length - num - 1));
+                if (c == '+')
+                    ans = a + b;
+                else if (c == '-')
+                    ans = a - b;
+                else if (c == '*')
+                    ans = a * b;
+                else if (c == '/')
+                    ans = (double)a / b;
+                else
+                {
+                    Console.WriteLine("Error");
+                    Console.ReadKey();
+                    return;
+                }
+                Console.WriteLine($"ans = {ans}");
+            }
+        }
+    }
+}
