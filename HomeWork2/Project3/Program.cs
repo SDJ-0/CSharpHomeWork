9dd52df625ae491f7edf05d1a23bbb5a16aa8b6d
JIAN DING <45487811+SDJ-0@users.noreply.github.com>
2020-02-24 16:48:54 +0800

    Add files via upload

---
+++ HomeWork2/Project3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number:");
+            int n;
+            try
+            {
+                n = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error Inpput!");
+                Console.ReadKey();
+                return;
+            }
+            bool[] Is_Prime = new bool[n + 1];
+            for (int i = 0; i < n + 1; i++)
+                Is_Prime[i] = true;
+            for (int i = 2; i * i <= n; i++) 
+                for (int j = 2; i * j <= n; j++)
+                {
+                    Is_Prime[i * j] = false;
+                }
+            for (int i = 2; i <= n; i++)
+                if (Is_Prime[i])
+                {
+                    Console.WriteLine(i);
+                }
+            Console.ReadKey();
+        }
+    }
+}
